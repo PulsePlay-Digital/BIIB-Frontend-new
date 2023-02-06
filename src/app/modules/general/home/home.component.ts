021c1669cf2fa4457250963b0a92248e838129f3
sahdev007 <sahdev.pathania@pulseplaydigital.com>
2023-02-06 13:01:49 +0530

    resolve mobile responsive issue and header links

--- src/app/modules/general/home/home.component.ts
+++ src/app/modules/general/home/home.component.ts
@@ -37,7 +37,7 @@ export class HomeComponent implements OnInit{
       {
         breakpoint: 1024,
         settings: {
-          slidesToShow: 3,
+          slidesToShow: 2,
           slidesToScroll: 1,
           infinite: true,
           dots: true
@@ -111,22 +111,20 @@ export class HomeComponent implements OnInit{
         breakpoint: 1024,
         settings: {
           slidesToShow: 3,
-          slidesToScroll: 1,
-          infinite: true,
-          dots: true
+          slidesToScroll: 1
         }
       },
       {
         breakpoint: 600,
         settings: {
           slidesToShow: 2,
-          slidesToScroll: 2
+          slidesToScroll: 1
         }
       },
       {
         breakpoint: 480,
         settings: {
-          slidesToShow: 1,
+          slidesToShow: 2,
           slidesToScroll: 1
         }
       }
