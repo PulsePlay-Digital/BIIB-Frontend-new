2f263146bf036d89fd3f4d6a701cf152119d15b3
sahdev007 <sahdev.pathania@pulseplaydigital.com>
2023-02-07 11:13:43 +0530

    resolve header link

--- src/app/modules/general/home/home.component.ts
+++ src/app/modules/general/home/home.component.ts
@@ -99,18 +99,23 @@ export class HomeComponent implements OnInit{
     autoplaySpeed: 2500,
     responsive: [
       {
+        breakpoint: 1200,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 1
+        }
+      },
+      {
         breakpoint: 1024,
         settings: {
           slidesToShow: 2,
           slidesToScroll: 1,
-          infinite: true,
-          dots: true
         }
       },
       {
         breakpoint: 600,
         settings: {
-          slidesToShow: 2,
+          slidesToShow: 1,
           slidesToScroll: 2
         }
       },
